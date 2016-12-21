d4123a693789afe0c5d7000c2150ec11953b4ab8
mattczyr <mattczyr@gmail.com>
2016-12-21 00:02:15 -0500

    Add auto-join feature

--- src/main/java/com/chaosthedude/autorefresh/gui/GuiAutoRefresh.java
+++ src/main/java/com/chaosthedude/autorefresh/gui/GuiAutoRefresh.java
@@ -19,23 +19,26 @@ public class GuiAutoRefresh extends GuiMultiplayer {
 
 	private GuiScreen parentScreen;
 	private GuiButton buttonAutoRefresh;
+	private GuiButton buttonAutoJoin;
 	private ServerSelectionList serverListSelector;
 	private boolean autoRefreshEnabled;
+	private boolean autoJoinEnabled;
 	private int ticksSinceRefresh;
 	private int selected;
 	private int amountScrolled;
 	private boolean hasAlerted;
 
-	public GuiAutoRefresh(GuiScreen parentScreen, int selected, int amountScrolled) {
+	public GuiAutoRefresh(GuiScreen parentScreen, int selected, int amountScrolled, boolean autoJoin) {
 		super(parentScreen);
 		this.parentScreen = parentScreen;
 		this.selected = selected;
 		this.amountScrolled = amountScrolled;
 		autoRefreshEnabled = true;
+		autoJoinEnabled = autoJoin;
 	}
 
 	public GuiAutoRefresh(GuiScreen parentScreen) {
-		this(parentScreen, -1, -1);
+		this(parentScreen, -1, -1, false);
 	}
 
 	@Override
@@ -54,8 +57,12 @@ public class GuiAutoRefresh extends GuiMultiplayer {
 	@Override
 	public void func_146794_g() {
 		super.func_146794_g();
-		buttonAutoRefresh = new GuiButton(8, width - 105, 5, 100, 20, I18n.format("string.autoRefresh") + ": " + I18n.format("string.on"));
+
+		buttonAutoRefresh = new GuiButton(9, width - 125, 5, 120, 20, I18n.format("string.autoRefresh") + ": " + I18n.format("string.on"));
 		buttonList.add(buttonAutoRefresh);
+
+		buttonAutoJoin = new GuiButton(10, 5, 5, 120, 20, I18n.format("string.autoJoin") + ": " + (autoJoinEnabled ? I18n.format("string.on") : I18n.format("string.off")));
+		buttonList.add(buttonAutoJoin);
 	}
 
 	@Override
@@ -68,12 +75,19 @@ public class GuiAutoRefresh extends GuiMultiplayer {
 			} else {
 				ticksSinceRefresh++;
 			}
-
-			if (!hasAlerted && serverListSelector != null) {
-				final int index = serverListSelector.func_148193_k();
-				if (index >= 0 && index < func_146795_p().countServers() && func_146795_p().getServerData(index).pingToServer > 0) {
+		}
+		
+		if (serverListSelector != null) {
+			final int index = serverListSelector.func_148193_k();
+			if (index >= 0 && index < func_146795_p().countServers() && func_146795_p().getServerData(index).pingToServer > 0) {
+				if (!hasAlerted) {
 					mc.getSoundHandler().playSound(PositionedSoundRecord.func_147674_a(new ResourceLocation("random.orb"), 1.0F));
 					hasAlerted = true;
+				}
+
+				if (autoJoinEnabled) {
+					func_146796_h();
+					toggleAutoJoin();
 				}
 			}
 		}
@@ -83,6 +97,8 @@ public class GuiAutoRefresh extends GuiMultiplayer {
 	protected void actionPerformed(GuiButton button) {
 		if (button == buttonAutoRefresh) {
 			toggleAutoRefresh();
+		} else if (button == buttonAutoJoin) {
+			toggleAutoJoin();
 		} else if (button.id == 8) {
 			refresh();
 		} else {
@@ -95,9 +111,14 @@ public class GuiAutoRefresh extends GuiMultiplayer {
 		buttonAutoRefresh.displayString = I18n.format("string.autoRefresh") + ": " + (autoRefreshEnabled ? I18n.format("string.on") : I18n.format("string.off"));
 	}
 
+	private void toggleAutoJoin() {
+		autoJoinEnabled = !autoJoinEnabled;
+		buttonAutoJoin.displayString = I18n.format("string.autoJoin") + ": " + (autoJoinEnabled ? I18n.format("string.on") : I18n.format("string.off"));
+	}
+
 	private void refresh() {
 		if (serverListSelector != null) {
-			mc.displayGuiScreen(new GuiAutoRefresh(parentScreen, serverListSelector.func_148193_k(), serverListSelector.getAmountScrolled()));
+			mc.displayGuiScreen(new GuiAutoRefresh(parentScreen, serverListSelector.func_148193_k(), serverListSelector.getAmountScrolled(), autoJoinEnabled));
 		} else {
 			mc.displayGuiScreen(new GuiAutoRefresh(parentScreen));
 		}
