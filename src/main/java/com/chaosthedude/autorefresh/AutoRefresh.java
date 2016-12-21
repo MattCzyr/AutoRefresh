b3b01c3b56b4cad567d58b855e4001c75562a6e0
mattczyr <mattczyr@gmail.com>
2016-12-21 00:01:47 -0500

    Increment version number

--- src/main/java/com/chaosthedude/autorefresh/AutoRefresh.java
+++ src/main/java/com/chaosthedude/autorefresh/AutoRefresh.java
@@ -19,7 +19,7 @@ public class AutoRefresh {
 
 	public static final String MODID = "autorefresh";
 	public static final String NAME = "Auto Refresh";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	@EventHandler
 	public void init(FMLPreInitializationEvent event) {
